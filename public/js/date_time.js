ce263cac3535e94b8ca11b5adaf3d64cb384bbb5
artemisoikonomou <59887252+artemisoikonomou@users.noreply.github.com>
2025-09-24 21:12:21 +0300

    Update date_time.js

--- public/js/date_time.js
+++ public/js/date_time.js
@@ -1,19 +1,9 @@
 //  THIS CODE IS USED FOR THE DATE  LIKE Thursday, June 19, 2025
  
- const d = new Date();
+//this is used for general date and time show
+  const d = new Date();
   const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
   document.getElementById("date").innerHTML = d.toLocaleString('en-US', options);
-
-  document.querySelectorAll(".titles").forEach(el => {
-    el.innerHTML = d.toLocaleString('en-US', options);
-  });
-
-  function handler(id) {
-    document.getElementById("title" + id).setAttribute("hidden", true);
-    document.getElementById("edit" + id).setAttribute("hidden", true);
-    document.getElementById("done" + id).removeAttribute("hidden");
-    document.getElementById("input" + id).removeAttribute("hidden");
-  }
 
 
 //THIS CODE IS USED FOR THE TIME LIKE  13:00 
@@ -34,4 +24,4 @@
     return i < 10 ? "0" + i : i;
   }
 
-  startTime(); // Don't forget to call it!
+  startTime(); 
